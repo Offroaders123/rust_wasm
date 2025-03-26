51d705c0edf5add0167e253f81eb95ca9673be13
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-03-25 17:53:46 -0700

    Node.js Build
    wasm-pack build --target nodejs
    node ./index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// @ts-check
+
+import { greet } from "./pkg/rust_wasm.js";
+
+console.log(greet());
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
+    #[wasm_bindgen(js_name = "console.log")]
     fn alert(s: &str);
 }
 
